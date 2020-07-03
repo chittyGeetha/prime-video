c30c030bf41d50c5c4d9f8b3bb1d08f0d8cfde57
chitty <geethavandadi123@gmail.com>
2020-07-03 12:15:04 +0530

    upload video

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from "react";
 import HeaderComponent from "./Components/Header/HeaderComponent";
 import { ToastContainer } from "react-toastify";
+import firebase from "./firebase";
 import "react-toastify/dist/ReactToastify.css";
 import {
   BrowserRouter as Router,
@@ -13,26 +14,57 @@ import Login from "./Components/Auth/LoginComponent";
 import Register from "./Components/Auth/RegisterComponent";
 import PasswordReset from "./Components/Auth/PasswordResetComponent";
 import PageNotFoundComponent from "./Components/PagenotFoundComponent/PageNotFoundComponent";
+import ListMovie from "./Components/PrimeDetails/ListMovie";
+import UploadVideoForm from "./Components/VideoComponent/UploadVideoForm";
+import ListVideo from "./Components/VideoComponent/ListsVideos";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      userData: "",
+    };
   }
+  // call firebase api
+  async componentDidMount() {
+    await firebase.auth().onAuthStateChanged((user) => {
+      console.log(user);
+      if (user) {
+        this.setState({ userData: user });
+        this.props.history.push("/");
+      } else {
+        this.setState({ userData: "" });
+        this.props.history.push("/login");
+      }
+    });
+  }
+
   render() {
     return (
       <Fragment>
         <Router>
           <header>
-            <HeaderComponent />
+            <HeaderComponent user={this.state.userData} />
           </header>
           <ToastContainer />
+
           <main>
             <Switch>
               <Route path="/" exact component={HomeComponent} />
-
               <Route path="/login" exact component={Login} />
               <Route path="/register" exact component={Register} />
               <Route path="/password-reset" exact component={PasswordReset} />
+              {this.state.userData ? (
+                <Fragment>
+                  <Route path="/list-movies" exact component={ListMovie} />
+                  <Route
+                    path="/upload-video"
+                    exact
+                    component={() => (
+                      <UploadVideoForm user={this.state.userData} />
+                    )}
+                  />
+                </Fragment>
+              ) : null}
               <Route path="**" component={PageNotFoundComponent} />
             </Switch>
           </main>
@@ -41,10 +73,4 @@ class App extends Component {
     );
   }
 }
-
 export default withRouter(App);
-
-/*--------
-App.js is the root component file and it holds global
- state for all sub-component
-*/
--- src/Components/HomeComponent/HomeComponent.jsx
+++ src/Components/HomeComponent/HomeComponent.jsx
@@ -1,40 +1,45 @@
-import React, { Component, Fragment } from 'react';
-import './HomeComponent.style.css';
-import {Link} from 'react-router-dom'
+import React, { Component, Fragment } from "react";
+import "./HomeComponent.style.css";
+import { Link } from "react-router-dom";
 class HomeComponent extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
-        let homePageStyles = {
-            "background-color": "#000",
-            "background-image":
-                "linear-gradient(to right, #000 40%, transparent 58%),url('https://m.media-amazon.com/images/G/31/AmazonVideo/2019/1102620_MLP_1440x675_apv189_V3._SY1200_FMJPG_.jpg')",
-            "background-position": "right top",
-            height: "100vh",
-        };
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-        return (  <Fragment>
-            <section className="mainBlock" style={homePageStyles}>
-                <div className="container-fluid">
-                    <div className="left">
-                        <h1 className="h1 text-white mb-2">Welcome to prime video</h1>
-                        <p className="text-white mt-3">
-                            Join Prime to watch the latest movies, TV shows and
-                            award-<br></br>winning Amazon Originals
-                         </p>
-                        <div className="regLink">
-                            <Link to="/register" className="text-white font-weight-bold h6">Start your 30-day free trial</Link>
+  render() {
+    let homePageStyles = {
+      "background-color": "#000",
+      "background-image":
+        "linear-gradient(to right, #000 40%, transparent 58%),url('https://m.media-amazon.com/images/G/31/AmazonVideo/2019/1102620_MLP_1440x675_apv189_V3._SY1200_FMJPG_.jpg')",
+      "background-position": "right top",
+      height: "100vh",
+    };
+
+    return (
+      <Fragment>
+        {/* container start here */}
+        <section className="mainBlock" style={homePageStyles}>
+                    <div className="container-fluid">
+                        <div className="left">
+                            <h1 className="h1 text-white mb-2">Welcome to prime video</h1>
+                            <p className="text-white mt-3">
+                                Join Prime to watch the latest movies, TV shows and
+                                award-<br></br>winning Amazon Originals
+                             </p>
+                            <div className="regLink">
+                                <Link to="/register" className="text-white font-weight-bold h6">Start your 30-day free trial</Link>
+                            </div>
+                        </div>
+                        <div className="right">
+                            <img src="bg.jpg" alt="" />
                         </div>
                     </div>
-                    <div className="right">
-                        <img src="bg.jpg" alt="" />
-                    </div>
-                </div>
-            </section>
-        </Fragment> );
-    }
+                </section>
+       
+      </Fragment>
+    );
+  }
 }
- 
+
 export default HomeComponent;
